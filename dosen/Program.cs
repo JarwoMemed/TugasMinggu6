d149c4cfb32d04b373a88acbb99ed18ca805f9c6
JarwoMemed <andipradita001@gmail.com>
2023-04-11 10:25:08 +0700

    Tugas Minggu ke 6

---
+++ dosen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dosen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dosen dosen = new dosen();
+
+            Console.WriteLine("Default dosen pengampu: ");
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Nama Dan Nik Dosen Baru:");
+            dosen.attdosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Mata Kuliah Baru:");
+            dosen.addcourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dosen/dosen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dosen
+{
+    class dosen
+    {
+        private int id, Nik;
+        private string nama, gender, course;
+    
+        public dosen()
+        {
+            this.id = 1;
+            this.Nik = 123456789;
+            this.nama = "Dosen Pengampu";
+            this.gender = "Male";
+            this.course = "Pemrograman Berorientasi Objek";
+        }
+        public void attdosen(string nama, int Nik)
+        {
+            this.nama = nama;
+            this.Nik = Nik;
+
+             
+        }
+
+        public void addcourse(string newcourse)
+        {
+            course += ", " + newcourse;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Nama: " + nama);
+            Console.WriteLine("NIK: " + Nik);
+            Console.WriteLine("gender: " + gender);
+            Console.WriteLine("course: " + course);
+        }
+    }
+}
